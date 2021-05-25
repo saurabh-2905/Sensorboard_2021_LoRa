81a819ea147aa130814915910147f5f3b00a50b4
MalavikaUnnikrishnan <63556279+MalavikaUnnikrishnan@users.noreply.github.com>
2021-05-25 20:58:54 +0200

    Update lopy.py

--- micropython_scripts/lopy/lopy.py
+++ micropython_scripts/lopy/lopy.py
@@ -4,7 +4,7 @@ from network import LoRa
 import socket
 
 lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868,
-            bandwidth=LoRa.BW_250KHZ, sf=10, preamble=4,
+            bandwidth=LoRa.BW_125KHZ, sf=7, preamble=8,
             coding_rate=LoRa.CODING_4_5, power_mode=LoRa.ALWAYS_ON,
             tx_iq=False, rx_iq=False, public=True) # Tried here also public=False, rx_iq=True
 
