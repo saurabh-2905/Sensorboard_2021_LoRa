6128990cf4b3a42c57bb4139485e1e345b724118
stechflo <#Tommy124356>
2021-05-31 17:08:12 +0200

    code from monday

--- micropython_scripts/lora_integration_main/main.py
+++ micropython_scripts/lora_integration_main/main.py
@@ -1,169 +1,187 @@
 #-------------------------------------------------------------------------------
-# author: Florian Stechmann
-# date: 31.03.2020
-# function: main.py die nach Durchführung der boot.py ausgeführt wird.
-# 	    Misst die Werte der Sensoren und bricht erst nach Abbruch durch
-# 	    Eingabe oder Stromentzg ab.
+# author: Malavika U, Florian Stechmann
+# date: 31.05.2020
+# function: 
 #-------------------------------------------------------------------------------
 
-from machine import Pin, I2C
-import ubinascii, machine, time
-
-from mcp3221 import MCP3221
+#import libraries
+from machine import Pin, I2C, SoftSPI
+import network, ustruct, time
 from scd30 import SCD30
+from lora import LoRa
+from mcp3221 import MCP3221
 from bmp180 import BMP180
 from am2301 import AM2301
 
-# Setzt die IP-Adresse des MQTT-Servers und die ID der MCU, sowie das WLAN objekt.
-WLAN = network.WLAN(network.STA_IF)
-MQTT_SERVER = "192.168.30.17"    
-CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-
-# Setzt Konstanten für die Adressen.
+#addresses of sensors
 O2_ADRR = const(0x48)
 CO_ADRR = const(0x49)
 SCD30_ADRR = const(0x61)
 AM2301_1_ADRR = const(0)
 AM2301_2_ADRR = const(4)
-AM2301_3_ADRR = const(17)  
+AM2301_3_ADRR = const(17)
 AM2301_4_ADRR = const(16)
 
-# Setzt Status der Verbindung initial auf 1 (verbunden)
-CONNECTION_O2 = 1
-CONNECTION_CO = 1
+#Connection_variables initialisation
+FAILED_LORA = 1
 CONNECTION_CO2 = 1
+CONNECTION_CO = 1
+CONNECTION_O2 = 1
 CONNECTION_BMP = 1
-CONNECTION_AM1 = 1
-CONNECTION_AM2 = 1
-CONNECTION_AM3 = 1
-CONNECTION_AM4 = 1
-CONNECTION_LORA = 1
+CONNECTION_A1 = 1
+CONNECTION_A2 = 1
+CONNECTION_A3 = 1
+CONNECTION_A4 = 1
+scd_co2 = 0
+scd_temp = 0
+scd_hum = 0
 
-# Erstellt ein von allen Sensoren zu nutzenden I2C-Port.
+#establish I2c Bus
 try:
-    I2CBUS = I2C(1, scl=Pin(22), sda=Pin(21), freq=100000)
+    I2CBUS = I2C(1, sda=Pin(21), scl= Pin(22), freq=100000)
 except:
-    raise
+    raise #set conn_variables to sensors zero ?
 
-# SPI Bus initialsierung
+#establish SPI Bus
 try:
-    SPI_BUS = SPI(1, baudrate=10000000, sck=Pin(18, Pin.OUT), mosi=Pin(23, Pin.OUT), miso=Pin(19, Pin.IN))
-    SPI_BUS.init()
-    lora_sender = LoRa(SPI_BUS, True, cs=Pin(5, Pin.OUT), rx=Pin(2, Pin.IN))
+    SPI_BUS = SoftSPI(baudrate=10000000, sck=Pin(18, Pin.OUT), mosi=Pin(23, Pin.OUT), miso=Pin(19, Pin.IN))
+    lora = LoRa(SPI_BUS, True, cs=Pin(5, Pin.OUT), rx=Pin(2, Pin.IN))
 except:
     FAILED_LORA = 0
 
-# Objekte der einzelnen Sensoren. Für genauerer Infos siehe Klassen der Sensoren.
-try:
-    mcp_o2 = MCP3221(I2CBUS, O2_ADRR)
-except:
-    CONNECTION_O2 = 0
-
-try:
-    mcp_co = MCP3221(I2CBUS, CO_ADRR)
-except:
-    CONNECTION_CO = 0
-    
 try:
     scd30 = SCD30(I2CBUS, SCD30_ADRR)
     scd30.start_continous_measurement()
 except:
     CONNECTION_CO2 = 0
-    
+
 try:
-    bmp180 = BMP180(I2CBUS)
+    MCP_CO = MCP3221(I2CBUS, CO_ADRR)
+except:
+    CONNECTION_CO = 0
+
+try:
+    MCP_O2 = MCP3221(I2CBUS, O2_ADRR)
+except:
+    CONNECTION_O2 = 0
+
+try:
+    BMP = BMP180(I2CBUS)
 except:
     CONNECTION_BMP = 0
-    
-try:
-    am2301_1 = AM2301(AM2301_1_ADRR)
-except:
-    CONNECTION_AM1 = 0
 
 try:
-    am2301_2 = AM2301(AM2301_2_ADRR)
+    AM2301_1 = AM2301(AM2301_1_ADRR)
 except:
-    CONNECTION_AM2 = 0
+    CONNECTION_A1 = 0
 
 try:
-    am2301_3 = AM2301(AM2301_3_ADRR)
+    AM2301_2 = AM2301(AM2301_2_ADRR)
 except:
-    CONNECTION_AM3 = 0
+    CONNECTION_A2 = 0
 
 try:
-    am2301_4 = AM2301(AM2301_4_ADRR)
+    AM2301_3 = AM2301(AM2301_3_ADRR)
 except:
-    CONNECTION_AM4 = 0
-    	
-def measure_SCD30():
+    CONNECTION_A3 = 0
+
+try:
+    AM2301_4 = AM2301(AM2301_4_ADRR)
+except:
+    CONNECTION_A4 = 0
+
+def measure_scd30():
     """
-    Liest die Werte des SCD30 aus.    
+    Takes CO2 reading
     """
     if scd30.get_status_ready() == 1:
         return scd30.read_measurement()
     else:
-        return ("F", "F", "F")
-   
-def measure_amb_press():
-    """
-    Liest den Druck von BMP aus.    
-    """
-    return bmp180.pressure/100
-
-def measure_o2():
-    """
-    """
-    return mcp_o2.read_measurement_o2()
+        return []
 
 def measure_co():
     """
+    Takes CO reading
     """
-    return mcp_co.read_measurement_co()
+    return MCP_CO.read_measurement_co()
+
+def measure_o2():
+    """
+    Takes O2 reading
+    """
+    return MCP_O2.read_measurement_o2()
+
+def measure_bmp():
+    """
+    Takes pressure reading
+    """
+    return BMP.pressure/100
 
 def measure_am1():
     """
+    Temp & humidity sensor 1 reading
     """
-    return am2301_1.read_measurement()
+    return AM2301_1.read_measurement()
 
 def measure_am2():
     """
+    Temp & humidity sensor 2 reading
     """
-    return am2301_2.read_measurement()
+    return AM2301_2.read_measurement()
 
 def measure_am3():
     """
+    Temp & humidity sensor 3 reading
     """
-    return am2301_3.read_measurement()
+    return AM2301_3.read_measurement()
 
 def measure_am4():
     """
+    Temp & humidity sensor 4 reading
     """
-    return am2301_4.read_measurement()
+    return AM2301_4.read_measurement()
 
-def measure_and_check(conn_stat, meas_func, number):
-    """
-    """
-    if conn_stat == 1:
-        try:
-            # TODO: SPEICHERALLOKATION OPTIMIEREN!
-            return meas_func()
-        except:
-            conn_stat = 0
-            return "F"+str(number)
-    else:
-        try:
-            # TODO: SPEICHERALLOKATION OPTIMIEREN!
-            ret = measu_func()
-            conn_stat = 1
-            return ret
-        except:
-            conn_stat = 0
-            return "F"
+CONNECTION_VAR = [1, 1, 1, 1, 1, 1, 1, 1]
+FUNC_VAR = (measure_scd30, measure_co, measure_o2, measure_bmp, measure_am1, measure_am2, measure_am3, measure_am4)
 
-# time for calibration
-time.sleep(10)
-
-# Endless loop
+#infinite loop execution
 while True:
-
+    FAIL_STATUS = 0
+    SENSOR_STATUS = 0
+    SENSOR_DATA = []
     
+    for i in range(len(CONNECTION_VAR)):
+        if CONNECTION_VAR[i]:
+            # Sensor Data is available & sensor is working
+            func_call = FUNC_VAR[i]
+            try:
+                if i == 0:
+                    # SCD30 sensor readings(involves three values)
+                    scd_co2, scd_temp, scd_hum = func_call()
+                    SENSOR_DATA.extend((round(scd_co2, 2), round(scd_temp, 2), round(scd_hum, 2)))
+                elif  i < 4 and i > 0:
+                    # MCP3221, BMP180 sensor reading
+                    SENSOR_DATA.append(round(func_call(), 2))
+                else:
+                    # AM2301 readings(involves 2 values)
+                    am_temp, am_hum = func_call()
+                    SENSOR_DATA.extend((round(am_temp, 2), round(am_hum, 2)))
+            except:
+                CONNECTION_VAR[i] = 0
+        if not CONNECTION_VAR[i]:
+            # Sensor failed
+            if i == 0:
+                SENSOR_DATA.extend((0,0,0)) # SCD30 involves three readings
+                FAIL_STATUS |= 1
+                SENSOR_STATUS = FAIL_STATUS
+            elif i < 4 and i > 0:
+                SENSOR_DATA.append(0) # Sensors other than SCD30
+                FAIL_STATUS = 1 << i
+                SENSOR_STATUS |= FAIL_STATUS
+            else:
+                SENSOR_DATA.extend((0,0)) # Sensors other than SCD30
+                FAIL_STATUS = 1 << i
+                SENSOR_STATUS |= FAIL_STATUS
+
+    msg = ustruct.pack('ffffffffffffffI', SENSOR_DATA[0], SENSOR_DATA[1], SENSOR_DATA[2], SENSOR_DATA[3], SENSOR_DATA[4], SENSOR_DATA[5], SENSOR_DATA[6], SENSOR_DATA[7], SENSOR_DATA[8], SENSOR_DATA[9], SENSOR_DATA[10], SENSOR_DATA[11], SENSOR_DATA[12], SENSOR_DATA[13], SENSOR_STATUS)
+    lora.send(msg)
