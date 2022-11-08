dde162010be4c457b14e4a7724d71fb736df5787
stechflo <#Tommy124356>
2022-11-08 16:16:52 +0100

    created and updated figures

--- pickle_data/analysis_red.py
+++ pickle_data/analysis_red.py
@@ -13,9 +13,8 @@ def calc_normal_pdr(data):
     pck_list_b1 = []
     for i in range(len(board1_data)):
         val = board1_data[i][1]
-        if val < 100:
-            pck_list_b1.append(val)
-    return len(pck_list_b1)/100
+        pck_list_b1.append(val)
+    return len(pck_list_b1)/pck_list_b1[len(pck_list_b1)-1]
 
 
 # non faulty boards pdr + print
@@ -72,45 +71,82 @@ for i in range(len(pr)):
     if pr[i][0] == 67:
         r = True
 
-print(pr)
+p_hb = pck_list_hb
+
+# calculate pdr for faulty board alone
+pdr_list = []
+for i in range(len(pl_1)):
+    pdr_list.append(pl_1[i][0])
+for i in range(len(pl_2)):
+    pdr_list.append(pl_2[i][0])
+
+pdr_faulty = len(pdr_list)/pdr_list[len(pdr_list)-1]
+
+# calculate pdr for faulty board with redundant board
+pdr_list2 = []
+for i in range(len(pr)):
+    pdr_list2.append(pr[i][0])
+pdr_list2 += pdr_list
+pdr_list2.sort()
+pdr_red = len(pdr_list2)/pdr_list2[len(pdr_list2)-1]
+
+print("PDR faulty: " + str(pdr_faulty))
+print("PDR redundancy: " + str(pdr_red))
+
+# create plot data
+# create x and y data for faulty board
+faulty_xdata = pl_1 + pl_2
+faulty_ydata = []
+for i in range(len(faulty_xdata)):
+    faulty_xdata[i] = faulty_xdata[i][1]
+    faulty_ydata.append("Tx")
+
+# create x and y data for hb signals
+hb_xdata = pck_list_hb
+hb_ydata = []
+for i in range(len(hb_xdata)):
+    hb_xdata[i] = hb_xdata[i][1]
+    hb_ydata.append("NoTx")
+
+# create x and ydata for redundancy board data msgs,
+# as well as x and y data for faulty board, when it is
+# not working
+red_xdata = pr
+red_ydata = []
+faulty2_xdata = []
+faulty2_ydata = []
+for i in range(len(red_xdata)):
+    red_xdata[i] = red_xdata[i][1]
+    red_ydata.append("Tx")
+    faulty2_xdata.append(red_xdata[i])
+    faulty2_ydata.append("NoTX")
 
 # plot data (or don't)
-plot = 0
+plot = 1
 if plot:
-    # create PB plot data
-    board2_xdata = []
-    board2_ydata = []
-    restart = 0
-    init_time = board2_data[0][3]
-    for i in range(len(board2_data)):
-        val = board2_data[i][3]
-        if val < 50:
-            restart += 1
-        if restart == 2:
-            val += 2400
-        board2_xdata.append(val)
-        board2_ydata.append("N")
-
-    # create RB plot data
-    red_xdata = []
-    red_ydata = []
-    for i in range(len(redundant_data)):
-        red_xdata.append(redundant_data[i][3])
-        if redundant_data[i][5][0] == -1.0:
-            red_ydata.append("R")
-        else:
-            red_ydata.append("N")
-    fig = plt.figure()
     plt.rcParams.update({'font.size': 12})
-    plt.plot(red_xdata, red_ydata, '+', color="green",
-             label="redundant node")
-    plt.plot(board2_xdata, board2_ydata, 'x', color="red",
-             label="primary node")
-    plt.yticks(["N", "R"], rotation=0)
-    plt.gca().invert_yaxis()
-    plt.xlim([0, 3000])
-    plt.ylabel("Status of the sensor nodes")
-    plt.xlabel("Time in seconds")
-    plt.grid(True)
-    plt.legend(bbox_to_anchor=(1, 1), loc="upper left")
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.grid(which='both')
+    ax1.set_xlim(0, 3000)
+    ax1.set_xlabel("time in seconds")
+    ax1.set_ylabel("primary board")
+    ax1.plot(faulty_xdata, faulty_ydata, '+', color="green",
+             label="transmission")
+    ax1.plot(faulty2_xdata, faulty2_ydata, '+', color="red",
+             label="no transmission")
+    ax1.legend(loc="center right")
+    ax1.invert_yaxis()
+
+    ax2.grid(which='both', zorder=0)
+    ax2.set_xlim(0, 3000)
+    ax2.set_xlabel("time in seconds")
+    ax2.set_ylabel("redundant board")
+    ax2.plot(hb_xdata, hb_ydata, 'x', color="red",
+             label="heartbeat transmission")
+    ax2.plot(red_xdata, red_ydata, 'x', color="green",
+             label="transmission")
+    ax2.legend(loc="center right")
+
+    plt.yticks(["NoTx", "Tx"], rotation=0)
     plt.show()
