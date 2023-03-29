7198e495434c5479179cc8ca88fe6e6cc22191cc
saurabh-2905 <saurabhband2905@gmail.com>
2023-03-29 17:28:40 +0200

    timing diag

--- pickle_data/timing_diag.py
+++ pickle_data/timing_diag.py
@@ -2,26 +2,95 @@
 # Script for analysing and plotting the data of Experiment 2 (Sensor Fault and Sensor Value Fault)
 
 
-from metrics import faulty_sensor, faulty_value_sensor
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
-prr_fs_all = []
-detection_fs_rate_all = []
+# load data for sensor value fault
+with open("log_exp_svf1.pkl", "rb") as f:
+    data = pickle.load(f)
 
-for i in range(1,4):
-    prr, dr = faulty_sensor.eval(f"log_exp_sf{i}.pkl")
-    prr_fs_all += [prr]
-    detection_fs_rate_all += [dr]
+### check if all packets belong to same board
+### PB
+pb_id = 0
+rb_id = 0
+for d_i, d in enumerate(data[1]):
+    if d_i == 0:
+        pb_id = d[1]
+        print('PB Id:',pb_id) 
+    assert(data[1][0][1]==d[1])
+### RB
+for d_i, d in enumerate(data[3]):
+    if d_i == 0:
+        rb_id = d[1]
+        print('RB Id:', rb_id)
+    assert(data[3][0][1]==d[1])
 
-print('PRR:', prr_fs_all)
-print('efficiency:', detection_fs_rate_all)
+### filter hb signals
+hb_pkts = []
+data_pkts = []
+board_index = [1,3]
 
-prr_fvs_all = []
-detection_fvs_rate_all = []
+for i in board_index:
+    each_board = data[i]
+    for d in each_board:
+        #print(d[0][0])
+        if d[0][0] == -1:
+            hb_pkts += [d]
+        else:
+            data_pkts += [d]
+            
 
-for i in range(1,4):
-    prr, dr = faulty_value_sensor.eval(f"log_exp_svf{i}.pkl")
-    prr_fvs_all += [prr]
-    detection_fvs_rate_all += [dr]
+#### Plotting timing diag ####
+primary_plotdata = []
+redundant_plotdata = []
 
-print('PRR:', prr_fs_all)
-print('efficiency:', detection_fs_rate_all)
+for d in data_pkts:
+    plotdata = [d[2], d[4], 0]   ### use the pkt id and tx time for plotting
+    if d[1] == pb_id:
+        if d[0][0] == 1000.0:
+            plotdata[2] = 1
+        primary_plotdata += [plotdata]
+    elif d[1] == rb_id:
+        redundant_plotdata += [plotdata]
+
+y_correct_pb = []
+y_faulty_pb = []
+x_correct_pb = []
+x_faulty_pb = []
+
+x_rb = []
+y_rb = []
+
+for d in primary_plotdata:
+    if d[-1] == 0:
+        y_correct_pb += [10]      
+        x_correct_pb += [d[1]]
+    else:
+        y_faulty_pb += [10]
+        x_faulty_pb += [d[1]]
+        
+for d in redundant_plotdata:
+    x_rb += [d[1]]
+    y_rb += [8]
+
+
+fig = plt.subplots(figsize =(20, 8))
+
+# Set position of bar on X axis
+#x_data = np.arange(primary_plotdata[-1][0])
+
+plt.scatter(x_correct_pb, y_correct_pb, label='correct data')
+plt.scatter(x_faulty_pb, y_faulty_pb, label='faulty data')
+plt.scatter(x_rb, y_rb, label='backup data')
+plt.ylim((7,11))
+plt.grid(True)
+plt.xlabel('Time (seconds)', fontsize = 15)
+plt.ylabel('Packet reception of resp. boards', fontsize = 15)
+plt.yticks([10,8],['Primary Board', 'Redundant Board'], rotation=90,)
+plt.legend()
+
+plt.show()
+
+
+
